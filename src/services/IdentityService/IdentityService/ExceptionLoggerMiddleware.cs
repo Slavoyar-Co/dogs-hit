7e3dbaf8ded44fe46ea67524c9b250317f0b766d
Coercivity <digitalpirouette@gmail.com>
2023-03-19 14:22:29 +0300

    #2 #19 add exception logger & code cleanup

---
+++ src/services/IdentityService/IdentityService/ExceptionLoggerMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using System.Security.Authentication;
+
+namespace IdentityService
+{
+    public class ExceptionLoggerMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly ILogger _logger;
+        public ExceptionLoggerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            this.next = next;
+            _logger = logger;   
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            HttpResponse response = context.Response;
+
+            try
+            {
+                await next(context);
+            }
+            catch (Exception error) 
+            {
+                switch (error) //todo add possible exceptions and handle them
+                {
+                    case AuthenticationException:
+                        
+                        if (context.User.Identity is { IsAuthenticated: true })
+                        {
+                        }
+
+                        break;
+                    default:
+                        // unhandled error
+                        response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                        break;
+                }
+            }
+            finally
+            {
+                if (context.Response.StatusCode == 404)
+                {
+                    _logger.LogInformation("404 Not Found. " + context.Request.Path.Value);
+                }
+            }
+        }
+    }
+}
